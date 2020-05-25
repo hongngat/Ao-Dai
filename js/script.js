52dade16cab17f4055a00d6b462d14babf9c5ba7
hongngat <48482193+hongngat@users.noreply.github.com>
2020-05-25 17:50:07 +0700

    A

--- js/script.js
+++ js/script.js
@@ -81,3 +81,12 @@ jQuery(function($) {
 
 
 })(window,jQuery);
+$(document).ready(function(){
+  $('a').click(
+    function(event){
+       $('html, body').animate({
+       scrollTop: $( $.attr(this, 'href') ).offset().top
+       }, 500);
+   event.preventDefault();
+  });
+});
